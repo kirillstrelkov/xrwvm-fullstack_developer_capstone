c96902e386e0e6bdc99fe52933fb39ac9fe6d9e9
Kirill Strelkov <7988855+kirillstrelkov@users.noreply.github.com>
2025-09-29 18:29:55 +0200

    fix Python files

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -50,8 +50,6 @@ def logout_request(request):
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
-
     # Load JSON data from the request body
     data = json.loads(request.body)
     username = data["userName"]
@@ -60,12 +58,11 @@ def registration(request):
     last_name = data["lastName"]
     email = data["email"]
     username_exist = False
-    email_exist = False
     try:
         # Check if user already exists
         User.objects.get(username=username)
         username_exist = True
-    except:
+    except Exception:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
@@ -126,10 +123,15 @@ def add_review(request):
     else:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+        except Exception as e:
+            return JsonResponse(
+                {
+                    "status": 401,
+                    "message": "Error in posting review: {}".format(str(e)),
+                }
+            )
 
 
 def get_cars(request):
@@ -140,5 +142,7 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append(
+            {"CarModel": car_model.name, "CarMake": car_model.car_make.name},
+        )
     return JsonResponse({"CarModels": cars})
