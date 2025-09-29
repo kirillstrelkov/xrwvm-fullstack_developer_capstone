35841fd37b5fee95482c8485e911416eb20d376c
Kirill Strelkov <7988855+kirillstrelkov@users.noreply.github.com>
2025-09-29 18:09:48 +0200

    add review

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -36,9 +36,8 @@ def analyze_review_sentiments(text):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
-        print(f"Unexpected {err=}, {type(err)=}")
-        print("Network exception occurred")
+    except Exception as e:
+        print("Error during requests to {} : {}".format(request_url, str(e)))
         return {"error": str(e)}
 
 
