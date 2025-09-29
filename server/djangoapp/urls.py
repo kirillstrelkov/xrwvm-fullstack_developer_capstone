b7b28356a67ad20adb21669f4ea037ff887e72f2
Kirill Strelkov <7988855+kirillstrelkov@users.noreply.github.com>
2025-09-29 13:04:23 +0200

    add registration

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # # path for registration
     # path for login
     path(route="login", view=views.login_user, name="login"),
+    path(route="logout", view=views.logout_request, name="logout"),
+    path(route="registration", view=views.registration, name="registration"),
     # path for dealer reviews view
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
